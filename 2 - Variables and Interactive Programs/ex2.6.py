66e05a5f8063bd9fae58fa5989f3a9e877664fd8
Gemmus <decadentcrown@gmail.com>
2022-08-26 09:52:55 +0300

    correction

--- 2 - Variables and Interactive Programs/ex2.6.py
+++ 2 - Variables and Interactive Programs/ex2.6.py
@@ -1,15 +1,15 @@
 import sys
 import random
 
-a = random.randrange(0, 9)
-b = random.randrange(0, 9)
-c = random.randrange(0, 9)
+a = random.randrange(0, 10)
+b = random.randrange(0, 10)
+c = random.randrange(0, 10)
 print("Your random 3-digit code is: ", a, b, c)
 
-c = random.randrange(1, 6)
-d = random.randrange(1, 6)
-e = random.randrange(1, 6)
-f = random.randrange(1, 6)
+c = random.randrange(1, 7)
+d = random.randrange(1, 7)
+e = random.randrange(1, 7)
+f = random.randrange(1, 7)
 print("You random 4-digit code is: ", c, d, e, f)
 
 sys.exit(0)
