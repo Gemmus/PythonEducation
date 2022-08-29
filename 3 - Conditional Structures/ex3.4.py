11b8ac20e0b3e5144283ebb54a80a5b6b35ee407
Gemmus <decadentcrown@gmail.com>
2022-08-29 11:15:45 +0300

    Correction.

--- 3 - Conditional Structures/ex3.4.py
+++ 3 - Conditional Structures/ex3.4.py
@@ -1,13 +1,15 @@
 import sys
 
-year = float(input("Please enter the year of your choice: "))
+year = int(input("Please enter the year of your choice: "))
 
 if year % 4 == 0:
     if year % 100 != 0:
         print("The year of your choice is a leap year.")
-    elif year % 100 == 0:
+    else:
         if year % 400 == 0:
             print("The year of your choice is a leap year.")
+        else:
+            print("The year of your choice is not a leap year.")
 else:
     print("The year of your choice is not a leap year.")
 
