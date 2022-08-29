c797b340444836d3a8555170b2119dc2449d400e
Gemmus <decadentcrown@gmail.com>
2022-08-29 10:24:08 +0300

    Minor correction 1.

--- 3 - Conditional Structures/ex3.4.py
+++ 3 - Conditional Structures/ex3.4.py
@@ -5,7 +5,7 @@ year = float(input("Please enter the year of your choice: "))
 if year % 4 == 0:
     if year % 100 != 0:
         print("The year of your choice is a leap year.")
-    if year % 100 == 0:
+    elif year % 100 == 0:
         if year % 400 == 0:
             print("The year of your choice is a leap year.")
 else:
