27b24aae886fe56d236c643407d00070ef109825
Gemmus <decadentcrown@gmail.com>
2022-08-30 21:22:19 +0300

    Attempt.

--- 4 - While Loops/ex4.3.py
+++ 4 - While Loops/ex4.3.py
@@ -1,1 +1,14 @@
-num= float(input("Please enter a number: "))
+largest = 0
+smallest = 0
+
+while True:
+    num = input("Please enter a number or press enter to quit: " )
+    if num == "":
+        break
+    num = float(num)
+    if num > largest:
+        largest = num
+    if num < smallest:
+        smallest = num
+
+print("Your smallest number is: " + str(smallest) + " and your largest is: " + str(largest))
