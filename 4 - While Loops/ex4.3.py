af1ca8df8bd27a6d144355cd4484204dce50bbb1
Gemmus <decadentcrown@gmail.com>
2022-08-31 09:12:00 +0300

    Correction.

--- 4 - While Loops/ex4.3.py
+++ 4 - While Loops/ex4.3.py
@@ -1,14 +1,19 @@
-largest = 0
-smallest = 0
+import sys
 
-while True:
-    num = input("Please enter a number or press enter to quit: " )
+list1 = []
+num = input("Please enter a number or press enter to quit: ")
+
+if num == "":
+    print("You chose to quit without entering a single number.")
+    sys.exit(0)
+
+list1.append(num)
+while num != "":
+    num = input("Please enter a number or press enter to quit: ")
     if num == "":
         break
-    num = float(num)
-    if num > largest:
-        largest = num
-    if num < smallest:
-        smallest = num
+    list1.append(num)
 
-print("Your smallest number is: " + str(smallest) + " and your largest is: " + str(largest))
+print("Your smallest number is: " + str(min(list1)) + " and your largest is: " + str(max(list1)))
+
+sys.exit(0)
--- 4 - While Loops/ex4.5.py
+++ 4 - While Loops/ex4.5.py
@@ -1,11 +1,15 @@
+import sys
+
 x = 0
 while x < 5:
     username = input("Please enter username: ")
     password = input("Please enter password: ")
     if username == "python" and password == "rules":
         print("Welcome.")
-        x = 6
+        sys.exit(0)
     x = x + 1
 
 if x == 5:
     print("Access denied.")
+
+    sys.exit(0)
