27a8391c097d67178fb6e39aac8ce9c5b171e692
Gemmus <decadentcrown@gmail.com>
2022-09-05 12:32:21 +0300

    Initial upload.

---
+++ 6 - Functions/ex6.3.py
@@ -0,0 +1,12 @@
+# Write a function that gets the quantity of gasoline in American gallons and returns the number converted to litres.
+# Write a main program that asks for a volume in gallons from the user and converts the value to liters.
+# The conversion must be done by using the function. Conversions continue until the user inputs a negative value.
+
+def litre(first):
+    result = first*3.7854
+    return result
+
+
+gallon = float(input("Please enter the volume in gallons: "))
+result = litre(gallon)
+print(f"The entered volume in gallon is {result:.2f} in litre.")
