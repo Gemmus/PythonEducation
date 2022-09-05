170e221242bb13c237e365a73ea826cfbc0a1108
Gemmus <decadentcrown@gmail.com>
2022-09-05 13:21:16 +0300

    Description of exercise has been corrected.

--- 6 - Functions/ex6.1.py
+++ 6 - Functions/ex6.1.py
@@ -1,5 +1,4 @@
-# Write a function that returns a random dice roll between 1 and 6.
-# The function should not have any parameters.
+# Write a function (without parameters) that returns a random dice roll between 1 and 6.
 # Write a main program that rolls the dice until the result is 6.
 # The main program should print out the result of each roll.
 
