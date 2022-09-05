23e407a355bc1d149e5ffc166fbcf8b1c3cb5005
Gemmus <decadentcrown@gmail.com>
2022-09-05 15:05:44 +0300

    Alternative added.

--- 6 - Functions/ex6.4.py
+++ 6 - Functions/ex6.4.py
@@ -9,3 +9,14 @@ def func(integers):
 list1 = [2, 98, 46, 6, 32, 37, 69, 294, 75]
 addition = func(list1)
 print(addition)
+
+# or
+
+
+def func(integers):
+    return sum(int(n) for n in integers)
+
+
+list1 = [2, 98, 46, "6", 32, 37, 69, "294", 75]
+addition = func(list1)
+print(addition)
