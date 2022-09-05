4d631aee0e809fcbeb3e355b0645e8c07cb404c7
Gemmus <decadentcrown@gmail.com>
2022-09-05 11:54:27 +0300

    Initial upload.

---
+++ 6 - Functions/ex6.1.py
@@ -0,0 +1,17 @@
+# Write a function that returns a random dice roll between 1 and 6.
+# The function should not have any parameters.
+# Write a main program that rolls the dice until the result is 6.
+# The main program should print out the result of each roll.
+
+
+def roll():
+    import random
+    dice = random.randint(1, 6)
+    return dice
+
+
+number = roll()
+print(number)
+while number != 6:
+    number = roll()
+    print(number)
