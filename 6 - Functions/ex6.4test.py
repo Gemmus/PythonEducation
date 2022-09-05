7f9425231e7aca83c3df19eab6d7b0fe2605afc2
Gemmus <decadentcrown@gmail.com>
2022-09-05 15:01:07 +0300

    Initial upload.

--- 6 - Functions/ex6.4test.py
+++ 6 - Functions/ex6.4test.py
@@ -3,9 +3,11 @@
 # For testing, write a main program where you create a list, call the function, and print out the value it returned.
 
 def func(integers):
-    return sum(integers)
+    print("You have the following numbers in your list: ")
+    for integer in integers:
+        print(integer)
+    return
 
 
 list1 = [2, 98, 46, 6, 32, 37, 69, 294, 75]
-addition = func(list1)
-print(addition)
+func(list1)
