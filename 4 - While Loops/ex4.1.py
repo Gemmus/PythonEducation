ad0ff9e9ad4cf67a6801ae82f2e5b29b800cef3d
Gemmus <decadentcrown@gmail.com>
2022-09-13 15:46:45 +0300

    Additional solution added.

--- 4 - While Loops/ex4.1.py
+++ 4 - While Loops/ex4.1.py
@@ -1,12 +1,17 @@
 # Write a program that uses a while loop to print out all numbers divisible by three in the range of 1-1000.
 
 number = 3
-
 while number <= 1000:
     print(number)
     number += 3
 
+# or
+number = 1
+while number <= 1000:
+    if number % 3 == 0:
+        print(number)
+    number += 1
 
-# with for loop:
+# or with for loop:
 # for number in range(3, 1000, 3):
 #    print(number)
