6df40bf8d43afa961136488615b5a36cd017d192
Gemmus <decadentcrown@gmail.com>
2022-09-14 18:14:11 +0300

    Try and except added.

--- 2 - Variables and Interactive Programs/ex2.2.py
+++ 2 - Variables and Interactive Programs/ex2.2.py
@@ -1,8 +1,12 @@
 # Write a program that asks for the radius of a circle and prints out the area of the circle.
 
 import math
-
-radius_str = input("Please insert radius of the circle: ")
-radius = float(radius_str)
-area = (radius**2)*math.pi
-print("The area of the circle is: " + str(area))
+try:
+    radius = float(input("Please insert radius of the circle: "))
+    if radius >= 0:
+        area = (radius**2)*math.pi
+        print(f"The area of the circle is: {area:.2f}")
+    else:
+        print("Value cannot be negative.")
+except ValueError:
+    print("Invalid input.")
--- 2 - Variables and Interactive Programs/ex2.3.py
+++ 2 - Variables and Interactive Programs/ex2.3.py
@@ -1,11 +1,15 @@
 # Write a program that asks for the length and width of a rectangle.
 # The program then prints out the perimeter and area of the rectangle.
 
-length_str = input("Please insert length of the rectangle: ")
-width_str = input("Please insert the width of the rectangle: ")
-length = float(length_str)
-width = float(width_str)
-perimeter = (length+width)*2
-area = (length*width)
-print("The perimeter of the rectangle is: " + str(perimeter))
-print("The area of the rectangle is: " + str(area))
+try:
+    length = float(input("Please insert length of the rectangle: "))
+    width = float(input("Please insert the width of the rectangle: "))
+    if length > 0 and width > 0:
+        perimeter = (length+width)*2
+        area = (length*width)
+        print(f"The perimeter of the rectangle is: {perimeter}")
+        print(f"The area of the rectangle is: {area}")
+    else:
+        print("Value cannot be negative.")
+except ValueError:
+    print("Invalid input.")
