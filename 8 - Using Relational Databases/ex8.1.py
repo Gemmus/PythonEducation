3ef79396c67037dacb51b9a3b668476678ec2cb0
Gemmus <decadentcrown@gmail.com>
2022-09-15 14:46:19 +0300

    Changed the variables related to DB.

--- 8 - Using Relational Databases/ex8.1.py
+++ 8 - Using Relational Databases/ex8.1.py
@@ -1,31 +1,30 @@
 # Write a program that asks the user to enter the ICAO code of an airport.
 # The program fetches and prints out the corresponding airport name and location (town)
 # from the airport database used on this course.
+# examples: EFHK (Helsinki-Vantaa), EGLL (Heathrow), EGKK (Gatwick), EGGW (Luton), EGSS (London Stansted)
 
 import mysql.connector
 
 
 def getNameAndLocation(icao):
-    sql = "SELECT id, name, municipality, gps_code FROM aiport"
-    sql += " WHERE gps_code='" + icao + "'"
-#    print(sql)
     cursor = connection.cursor()
-    cursor.execute(sql)
+    cursor.execute("SELECT name, municipality, gps_code FROM flying_around WHERE gps_code='" + icao + "'")
     result = cursor.fetchall()
+#    print(result)
     if cursor.rowcount > 0:
         for row in result:
-            print(f"The name of the airport is: {row[1]} and it is located in: {row[2]}.")
+            print(f"The name of the airport is: {row[0]} and it locates in: {row[1]}.")
     return
 
 
 connection = mysql.connector.connect(
          host='127.0.0.1',
          port=3306,
-         database='flight_game',
+         database='for_python_exercise',
          user='root',
          password='CamdenTown',
          autocommit=True
          )
 
-code = input("Please enter the ICAO code of the airport: ")
+code = input("Please enter the ICAO code of the airport: ").upper()
 getNameAndLocation(code)
--- 8 - Using Relational Databases/ex8.2.py
+++ 8 - Using Relational Databases/ex8.2.py
@@ -6,26 +6,24 @@ import mysql.connector
 
 
 def getTypeAndName(code):
-    sql = "SELECT id, type, name, iso_country FROM airport"
-    sql += " WHERE iso_country='" + code + "' ORDER BY type ASC"
-#    print(sql)
     cursor = connection.cursor()
-    cursor.execute(sql)
+    cursor.execute("SELECT type, name, iso_country FROM flying_around "
+                   "WHERE iso_country='" + code + "' ORDER BY type DESC")
     result = cursor.fetchall()
     if cursor.rowcount > 0:
         for row in result:
-            print(f"The type of airport is: {row[1]} and the name of the airport is: {row[2]}.")
+            print(f"The type of airport is: {row[0]} and the name of the airport is: {row[1]}.")
     return
 
 
 connection = mysql.connector.connect(
          host='127.0.0.1',
          port=3306,
-         database='people',
+         database='for_python_exercise',
          user='root',
          password='CamdenTown',
          autocommit=True
          )
 
-area_code = input("Please enter the area code of the airport: ")
+area_code = input("Please enter the area code of the airport: ").upper()
 getTypeAndName(area_code)
