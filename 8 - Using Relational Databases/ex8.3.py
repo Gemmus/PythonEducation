b7406a8a6eab7a25b66a5b6d5af3c2699ae87f63
Gemmus <decadentcrown@gmail.com>
2022-09-15 17:12:39 +0300

    Initial upload.

--- 8 - Using Relational Databases/ex8.3.py
+++ 8 - Using Relational Databases/ex8.3.py
@@ -1,18 +1,35 @@
 # Write a program that asks the user to enter the ICAO codes of two airports.
 # The program prints out the distance between the two airports in kilometers.
-# The calculation is based on the airport coordinates fetched from the database.
-# Calculate the distance using the geopy library: https://geopy.readthedocs.io/en/stable/.
-# Install the library by selecting View / Tool Windows / Python Packages in your PyCharm IDE,
-# write geopy into the search field and finish the installation.
+# The calculation is based on the airport coordinates fetched from the database using geopy.
+# examples: EFHK (Helsinki-Vantaa), EGLL (Heathrow), EGKK (Gatwick), EGGW (Luton), EGSS (London Stansted)
 
 import mysql.connector
+from geopy.distance import geodesic
 
-yhteys = mysql.connector.connect(
+
+def howFar(icao):
+    cursor = connection.cursor()
+    cursor.execute("SELECT latitude_deg, longitude_deg, gps_code FROM flying_around WHERE gps_code='" + icao + "'")
+    result = cursor.fetchall()
+    if cursor.rowcount > 0:
+        for row in result:
+            location = (row[0], row[1])
+            return location
+
+
+connection = mysql.connector.connect(
          host='127.0.0.1',
          port=3306,
-         database='people',
-         user='dbuser',
-         password='pAs5w_0rD',
+         database='for_python_exercise',
+         user='root',
+         password='CamdenTown',
          autocommit=True
          )
 
+code1 = input("Please enter the ICAO code of the airport: ").upper()
+location1 = howFar(code1)
+code2 = input("Please enter the ICAO code of the airport: ").upper()
+location2 = howFar(code2)
+
+distance = geodesic(location1, location2).km
+print(f"The distance between the two entered airports is: {distance:.2f}km.")
