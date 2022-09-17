9a805a2baf54b6f85148ea6342e1663f4dab761f
Gemmus <decadentcrown@gmail.com>
2022-09-17 12:58:10 +0300

    Try and except added.

--- 6 - Functions/ex6.3.py
+++ 6 - Functions/ex6.3.py
@@ -2,15 +2,19 @@
 # Write a main program that asks for a volume in gallons from the user and converts the value to liters.
 # The conversion must be done by using the function. Conversions continue until the user inputs a negative value.
 
-def litre(first):
-    result = first * 3.7854
-    return result
+try:
+    def litre(first):
+        outcome = first * 3.7854
+        return outcome
 
 
-gallon = float(input("Please enter the volume in gallons: "))
-while gallon > 0:
-    result = litre(gallon)
-    print(f"The entered volume in gallon is {result:.2f} in litre.")
     gallon = float(input("Please enter the volume in gallons: "))
-else:
+    while gallon > 0:
+        result = litre(gallon)
+        print(f"The entered volume in gallon is {result:.2f} in litre.")
+        gallon = float(input("Please enter the volume in gallons: "))
+    else:
+        print("Invalid input.")
+
+except ValueError:
     print("Invalid input.")
