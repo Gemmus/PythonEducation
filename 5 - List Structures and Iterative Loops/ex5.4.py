a8c2bdd3c88545bf42be8c36ca6d44f4efc7f19b
Gemmus <decadentcrown@gmail.com>
2022-09-17 13:31:09 +0300

    Alternative solution added.

--- 5 - List Structures and Iterative Loops/ex5.4.py
+++ 5 - List Structures and Iterative Loops/ex5.4.py
@@ -7,5 +7,10 @@ for n in range(0, 5):
     city = input("Please enter the name of a city: ")
     city_list.append(city)
 
-for n in range(len(city_list)):
-    print(city_list[n])
+# for n in range(len(city_list)):
+#     print(city_list[n])
+
+# or
+
+for n in city_list:
+    print(n)
