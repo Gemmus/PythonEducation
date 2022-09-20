09275e94e0cc0e48746c62ad3fa8c154d7eaef14
Gemmus <decadentcrown@gmail.com>
2022-09-20 14:23:23 +0300

    Initial upload.

--- 10 - Association/ex10.1.py
+++ 10 - Association/ex10.1.py
@@ -8,13 +8,31 @@
 # tell it to move to a floor of your choice and then back to the bottom floor.
 
 class Elevator:
-    def __init__(self, bottom= 1, top):
+    def __init__(self, bottom=1, top=10, current=1):
         self.bottom = bottom
         self.top = top
+        self.current = current
 
-    def go_to_floor(self,):
+    def go_to_floor(self, floor):
+        if self.bottom <= floor <= self.top:
+            self.current = floor
+        return self.current
 
-    def floor_up(self,):
+    def floor_up(self):
+        if self.current < self.top:
+            self.current += 1
+        return self.current
 
-    def floor_down(self,):
+    def floor_down(self):
+        if self.current > self.bottom:
+            self.current -= 1
+        return self.current
 
+
+h = Elevator()
+print(h.go_to_floor(5))
+for i in range(3):
+    h.floor_up()
+print(h.current)
+print(h.floor_down())
+print(h.go_to_floor(10))
