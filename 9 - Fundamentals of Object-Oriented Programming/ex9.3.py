911ac6a55bea15bf1a24143a1b7fe9c01271d177
Gemmus <decadentcrown@gmail.com>
2022-10-27 09:15:18 +0300

    Extension of original code.

--- 9 - Fundamentals of Object-Oriented Programming/ex9.3.py
+++ 9 - Fundamentals of Object-Oriented Programming/ex9.3.py
@@ -3,26 +3,45 @@
 # Example: The travelled distance of car object is 2000 km. The current speed is 60 km/h.
 # Method call car.drive(1.5) increases the travelled distance to 2090 km.
 
-class Vehicle:
-    def __init__(self, plate, maximum_speed, current_speed=0, distance=0):
-        self.plate = plate
+class Car:
+    def __init__(self, registration_number, maximum_speed, current_speed=0, travelled_distance=0):
+        self.registration_number = registration_number
         self.maximum_speed = maximum_speed
         self.current_speed = current_speed
-        self.distance = distance
+        self.travelled_distance = travelled_distance
 
-    def number_of_hours(self, hour=0):
-        self.distance = self.distance + hour * 60
+    def accelerate(self, change_of_speed):
+        self.current_speed += change_of_speed
+        if self.current_speed < 0:
+            self.current_speed = 0
+        elif self.current_speed > self.maximum_speed:
+            self.current_speed = self.maximum_speed
+        return
+
+    def drive(self, number_of_hours):
+        for i in range(number_of_hours):
+            self.travelled_distance += self.current_speed
+        return
 
 
-car = Vehicle("ABC-123", 142, 60, 2000)
-car.number_of_hours(+5)     # +300km
-print(f"{car.distance:d}")
-car.number_of_hours(+9)     # +540 km
-print(f"{car.distance:d}")
-car.number_of_hours(+200)     # +12000 km
-print(f"{car.distance:d}")
-
-
+car1 = Car("ABC-123", 142)
+car1.accelerate(+90)
+# print(f"{car1.current_speed} km/h * 5 hours")
+car1.drive(5)
 print(f"""
-The car with the registration plate {car.plate:s} has the maximum speed of {car.maximum_speed:d} km/h.
-The current speed is {car.current_speed:d} km/h and has travelled {car.distance:d} km.""")
+The car with the registration plate {car1.registration_number:s} has the maximum speed of {car1.maximum_speed:d} km/h.
+The current speed is {car1.current_speed:d} km/h and has travelled {car1.travelled_distance:d} km.""")
+
+car1.accelerate(-20)
+# print(f"\n{car1.current_speed} km/h * 10 hours")
+car1.drive(10)
+print(f"""
+The car with the registration plate {car1.registration_number:s} has the maximum speed of {car1.maximum_speed:d} km/h.
+The current speed is {car1.current_speed:d} km/h and has travelled {car1.travelled_distance:d} km.""")
+
+car1.accelerate(+30)
+# print(f"\n{car1.current_speed} km/h * 200 hours")
+car1.drive(200)
+print(f"""
+The car with the registration plate {car1.registration_number:s} has the maximum speed of {car1.maximum_speed:d} km/h.
+The current speed is {car1.current_speed:d} km/h and has travelled {car1.travelled_distance:d} km.""")
