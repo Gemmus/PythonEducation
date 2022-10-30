4816009b803e27a039193173b0593c9cf1927fe8
Gemmus <decadentcrown@gmail.com>
2022-10-30 06:17:10 +0200

    Source of algorithm added to out-commented text.

--- 5 - List Structures and Iterative Loops/ex5.3.py
+++ 5 - List Structures and Iterative Loops/ex5.3.py
@@ -1,4 +1,6 @@
 # Write a program that asks the user for an integer and tells if the number is a prime number.
+# Implemented based on the algorithm from wikipedia:
+# https://en.wikipedia.org/wiki/Leap_year#/media/File:Leap_Year_Algorithm.png
 
 try:
     num = int(input("Please enter an integer number: "))
