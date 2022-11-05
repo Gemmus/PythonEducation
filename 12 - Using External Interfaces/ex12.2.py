dc662de7ec7f9e53687b81e835ec610c8a13a897
Gemmus <decadentcrown@gmail.com>
2022-11-05 09:19:27 +0200

    Initial upload.

--- 12 - Using External Interfaces/ex12.2.py
+++ 12 - Using External Interfaces/ex12.2.py
@@ -1,32 +1,20 @@
-# Familiarize yourself with the OpenWeather weather API at: https://openweathermap.org/api.
+# Familiarize yourself with the OpenWeather weather API.
 # Your task is to write a program that asks the user for the name of a municipality
 # and then prints out the corresponding weather condition description text and temperature in Celsius degrees.
-# Take a good look at the API documentation.
-# You must register for the service to receive the API key required for making API requests.
-# Furthermore, find out how you can convert Kelvin degrees into Celsius.
 
 import requests
-import json
 
-municipality = input("Please enter the municipality: ")
+city_name = input("Please enter the municipality(City,CountryCode): ")
+api_key = "628dc836c467279560786b9ec5b2a731"
 
-request = "https://api.tvmaze.com/search/shows?q=" + municipality
-response = requests.get(request).json()
-print(response)
-# or:
-print(json.dumps(response, indent=2))
-# or:
-for a in response:
-    print(a["show"]["name"])
-# or:
-try:
-    response = requests.get(request)
-    if response.status_code == 200:
-        json_response = response.json()
-        # print(json.dumps(json_response, indent=2))
-        for a in json_response:
-            print(a["show"]["name"])
-except requests.exceptions.RequestException as e:
-    print("Request could not be completed.")
 
-# C = K - 273.15 K = C + 273.15
+def get_weather(api, city):
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api}"
+
+    response = requests.get(url).json()
+    temp = response['main']['temp']
+    celsius = temp - 273.15
+    print(f"It is currently {celsius:4.2f}°C in {city}.")
+
+
+get_weather(api_key, city_name)
