16a9545849e3ca23aa58154ec385db973aa1ab6f
Gemmus <decadentcrown@gmail.com>
2022-11-23 14:56:13 +0200

    Correction to fit the instruction of the exercise better.

--- 9 - Fundamentals of Object-Oriented Programming/ex9.4.py
+++ 9 - Fundamentals of Object-Oriented Programming/ex9.4.py
@@ -16,42 +16,47 @@
 import random
 
 
-class Race:
-    def __init__(self, registration_number, current_speed, travelled_distance=0):
+class Car:
+    def __init__(self, registration_number, max_speed, current_speed=0, travelled_distance=0):
         self.registration_number = registration_number
+        self.max_speed = max_speed
         self.current_speed = current_speed
         self.travelled_distance = travelled_distance
 
     def accelerate(self, change_of_speed):
         self.current_speed += change_of_speed
-        if self.current_speed < 100:
-            self.current_speed = 100
-        elif self.current_speed > 200:
-            self.current_speed = 200
+        if self.current_speed < 0:
+            self.current_speed = 0
+        elif self.current_speed > self.max_speed:
+            self.current_speed = self.max_speed
         return self.current_speed
 
-    def drive(self, addition):
-        self.travelled_distance += addition
+    def drive(self, hours):
+        self.travelled_distance += hours*self.current_speed
         return self.travelled_distance
 
 
 car_list = []
 plate_number = 1
 for i in range(10):
-    speed = random.randint(100, 200)
-    new_car = Race(plate_number, speed)
-    car_list.append(new_car)
+    random_max_speed = random.randint(100, 200)
+    initial_speed = random.randint(100, random_max_speed)
+    car = Car(plate_number, random_max_speed, initial_speed)
+    car_list.append(car)
     plate_number += 1
 
 race_over = False
 circle = 1
 while not race_over:
-    print("\nRound " + str(circle) + ":")
+    print(f"\nRound {circle}:")
     for car in car_list:
-        acceleration = random.randint(-10, +15)
-        speed = car.accelerate(acceleration)
-        distance = car.drive(speed)
-        print("[ABC-" + str(car.registration_number) + "] speed: " + str(speed) + " km/h  distance: " + str(distance) + " km")
-        if distance >= 10000:
+        car.accelerate(random.randint(-10, +15))
+        car.drive(1)
+        print(f"[ABC-{car.registration_number}] maximum speed: {car.max_speed}km/h, current speed: {car.current_speed}km/h, travelled distance: {car.travelled_distance}km")
+        if car.travelled_distance > 10000:
             race_over = True
     circle += 1
+
+print("\nThe result is:")
+for car in car_list:
+    print(f"[ABC-{car.registration_number}] maximum speed: {car.max_speed}km/h, current speed: {car.current_speed}km/h, travelled distance: {car.travelled_distance}km")
